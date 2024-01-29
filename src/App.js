66c013d6aa6d2b72fbf4f47f67b7df981ac63d59
Subha85829 <subhasishmodak27@gmail.com>
2024-01-29 14:59:31 +0530

    First Phase of UI and Chat Engine.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-import { ChatEngine } from "react-chat-engine";
+import { ChatEngine, ChatFeed } from "react-chat-engine";
 
 import './App.css';
 
+const App = () => {
+    return(
+        <ChatEngine
+            height = "100vh"
+            projectID = "db803362-6ee5-4d49-bed1-ab0014582082"
+            userName = "Sub_thehuman"
+            userSecret = "1234"
+        />
+    )
+}
+
+export default App;
